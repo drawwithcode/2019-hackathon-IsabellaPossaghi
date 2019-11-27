b8b21302fa0dc38b2a8955b6a95967412152d87a
Isabella Possaghi <isabella.possaghi@gmail.polimi.it>
2019-11-27 11:58:09 +0100

    updates

--- sketch.js
+++ sketch.js
@@ -28,6 +28,7 @@ function preload() {
   earth_2 = loadImage("./assets/earth_2.jpg")
   earth_3 = loadImage("./assets/earth_3.jpg")
   earth_4 = loadImage("./assets/earth_4.jpg")
+  earth_enter = loadImage("./assets/earth_enter.jpg")
 }
 
 function setup() {
@@ -49,6 +50,7 @@ function draw() {
   t = t + 0.15;
   //z variables for little squares
   z = z + 0.05;
+  z1 = z + 0.01;
 
   var volume = 0;
   if (mouseIsPressed) {
@@ -73,6 +75,7 @@ function draw() {
 
       image(earth, windowWidth / 2, windowHeight, 1200, 700)
       image(earth_2, windowWidth / 2, windowHeight / 5, 1000, 400)
+
 
       //create a grid of little squares
       for (var x = 20; x < windowWidth; x += 120) {
@@ -121,22 +124,27 @@ function draw() {
       image(earth_3, windowWidth / 2, windowHeight, 1200, 700)
       image(earth_4, windowWidth / 2, windowHeight / 5, 1000, 400)
 
+
       push()
       translate(width / 2, height / 2);
-      rotate(frameCount / 180);
-      rot = frameCount / 180;
+      rotate(frameCount / 90);
+      rot = frameCount / 90;
 
-      image(tg1_bianco, 0, 0 + sin(z) * 1, 340, 230);
-      image(tg1_nero, 0, 0 + sin(z) * -10, 340, 230);
-      image(tg1_bianco, 0, 0 + sin(z) * -20, 340, 230);
-      image(tg1_nero, 0, 0 + sin(z) * -30, 340, 230);
-      image(tg1_bianco, 0, 0 + sin(z) * -40, 340, 230);
-      image(tg1_nero, 0, 0 + sin(z) * -50, 340, 230);
-      image(tg1_bianco, 0, 0 + sin(z) * -60, 340, 230);
+      image(tg1_bianco, 0, 0 + sin(z1/5) * 1, 340, 230);
+      image(tg1_nero, 0, 0 + sin(z1/5) * -10, 340, 230);
+      image(tg1_bianco, 0, 0 + sin(z1/5) * -20, 340, 230);
+      image(tg1_nero, 0, 0 + sin(z1/5) * -30, 340, 230);
+      image(tg1_bianco, 0, 0 + sin(z1/5) * -40, 340, 230);
+      image(tg1_nero, 0, 0 + sin(z1/5) * -50, 340, 230);
+      image(tg1_bianco, 0, 0 + sin(z1/5) * -60, 340, 230);
 
       pop()
     }
   } else {
+
+    image(earth_enter,width/2,height/2,300,300)
+
+
     var myText = "Press F to enter"
     textFont("Share Tech Mono");
     textSize(50);
